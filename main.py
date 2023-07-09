f5fe0bb1bcc077f9495540fd610f776f8a8548e0
Your Name <you@example.com>
2023-07-09 15:36:11 +0530

    Added paddle class. Code to move Paddle between 0 and width of the display window

--- main.py
+++ main.py
@@ -1,17 +1,40 @@
 import pygame
 
-width, height = 800, 600
-win = pygame.display.set_mode((width, height))
+WIDTH, HEIGHT = 800, 600
+win = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Brick Breaker")
 FPS = 60
 
-def draw():
+PADDLE_WIDTH = 100
+PADDLE_HEIGHT = 15
+
+
+class Paddle:
+    VEL = 5
+
+    def __init__(self, x, y, width, height, color):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+
+    def draw(self, win):
+        pygame.draw.rect(win, self.color    , (self.x, self.y, self.width, self.height))
+
+    def move(self, direction=1):
+        self.x += self.VEL * direction
+
+
+def draw(win, paddle):
     win.fill("white")
+    paddle.draw(win)
     pygame.display.update()
 
 
 def main():
     clock = pygame.time.Clock()
+    paddle = Paddle(WIDTH//2 - PADDLE_WIDTH//2, HEIGHT - PADDLE_HEIGHT - 5, PADDLE_WIDTH, PADDLE_HEIGHT, "black")
     run = True
     while run:
         clock.tick(FPS)
@@ -20,7 +43,14 @@ def main():
                 run = False
                 break
 
-            draw()
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and paddle.x - paddle.VEL >= 0:
+            paddle.move(-1)
+
+        if keys[pygame.K_RIGHT] and paddle.x + paddle.width + paddle.VEL <= WIDTH:
+            paddle.move(1)
+
+        draw(win, paddle)
 
     pygame.quit()
     quit()
