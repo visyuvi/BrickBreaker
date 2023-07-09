dd917055f57ddf5309b444e20451d674ac56857f
Your Name <you@example.com>
2023-07-09 15:06:20 +0530

    Added clock  and draw function. Background color is set to white

--- main.py
+++ main.py
@@ -3,15 +3,24 @@ import pygame
 width, height = 800, 600
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Brick Breaker")
+FPS = 60
+
+def draw():
+    win.fill("white")
+    pygame.display.update()
 
 
 def main():
+    clock = pygame.time.Clock()
     run = True
     while run:
+        clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+            draw()
 
     pygame.quit()
     quit()
