72be4b9b0dee6a0426d126945ea29fabd301b36e
Your Name <you@example.com>
2023-07-09 15:56:25 +0530

    Added Ball class. Ball to move in upward direction

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ FPS = 60
 
 PADDLE_WIDTH = 100
 PADDLE_HEIGHT = 15
+BALL_RADIUS = 10
 
 
 class Paddle:
@@ -20,21 +21,51 @@ class Paddle:
         self.color = color
 
     def draw(self, win):
-        pygame.draw.rect(win, self.color    , (self.x, self.y, self.width, self.height))
+        pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.height))
 
     def move(self, direction=1):
         self.x += self.VEL * direction
 
 
-def draw(win, paddle):
+class Ball:
+    VEL = 5
+
+    def __init__(self, x, y, radius, color):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.x_vel = 0
+        self.y_vel = -self.VEL
+
+    def move(self, ):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+    def set_vel(self, x_vel, y_vel):
+        self.x_vel = x_vel
+        self.y_vel = y_vel
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
+
+def draw(win, paddle, ball):
     win.fill("white")
     paddle.draw(win)
+    ball.draw(win)
     pygame.display.update()
 
 
 def main():
     clock = pygame.time.Clock()
-    paddle = Paddle(WIDTH//2 - PADDLE_WIDTH//2, HEIGHT - PADDLE_HEIGHT - 5, PADDLE_WIDTH, PADDLE_HEIGHT, "black")
+
+    paddle_x = WIDTH // 2 - PADDLE_WIDTH // 2
+    paddle_y = HEIGHT - PADDLE_HEIGHT - 5
+
+    paddle = Paddle(paddle_x, paddle_y, PADDLE_WIDTH, PADDLE_HEIGHT, "black")
+    ball = Ball(WIDTH//2, paddle_y - BALL_RADIUS, BALL_RADIUS, "black")
+
     run = True
     while run:
         clock.tick(FPS)
@@ -50,7 +81,8 @@ def main():
         if keys[pygame.K_RIGHT] and paddle.x + paddle.width + paddle.VEL <= WIDTH:
             paddle.move(1)
 
-        draw(win, paddle)
+        ball.move()
+        draw(win, paddle, ball)
 
     pygame.quit()
     quit()
